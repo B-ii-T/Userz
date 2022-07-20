b7c1c48b9c03921bb8cf2c8d29ccc52cc0fff47b
Your Name <you@example.com>
2022-07-20 18:50:29 +0100

    Referring to the view model from the main activity commit

--- app/src/main/java/com/bit/userz/MainActivity.java
+++ app/src/main/java/com/bit/userz/MainActivity.java
@@ -1,14 +1,29 @@
 package com.bit.userz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AccountViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(AccountViewModel.class);
+        viewModel.getAllAccounts().observe(this, new Observer<List<Account>>() {
+            @Override
+            public void onChanged(List<Account> accounts) {
+                //updating he recycler view
+                Toast.makeText(MainActivity.this, "updating recycler view", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
