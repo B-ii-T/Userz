7225290e375275e9b41ecaece704d057e5551538
Your Name <you@example.com>
2022-07-21 16:09:11 +0100

    Recycler view + Accounts adapter

---
+++ app/src/main/java/com/bit/userz/AccountAdapter.java
@@ -0,0 +1,52 @@
+package com.bit.userz;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountAdapter extends RecyclerView.Adapter<AccountAdapter.AccountHolder> {
+    private List<Account> accounts = new ArrayList<>();
+    @NonNull
+    @Override
+    public AccountHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.account_sample, parent, false);
+        return new AccountHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AccountHolder holder, int position) {
+        Account currentAccount = accounts.get(position);
+        holder.username.setText(currentAccount.getUsername());
+        holder.platform.setText(currentAccount.getPlatform());
+        holder.appIcon.setImageResource(currentAccount.getIcon());
+    }
+
+    @Override
+    public int getItemCount() {
+        return accounts.size();
+    }
+
+    public void setAccounts(List<Account> accounts){
+        this.accounts = accounts;
+        notifyDataSetChanged();
+    }
+
+    class AccountHolder extends RecyclerView.ViewHolder{
+        private TextView username, platform, creationDate;
+        private ImageView appIcon;
+        public AccountHolder(@NonNull View itemView) {
+            super(itemView);
+            username = itemView.findViewById(R.id.username);
+            platform = itemView.findViewById(R.id.platform);
+            appIcon = itemView.findViewById(R.id.app_icon);
+        }
+    }
+}
--- app/src/main/java/com/bit/userz/MainActivity.java
+++ app/src/main/java/com/bit/userz/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import java.util.List;
@@ -15,14 +19,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final AccountAdapter adapter = new AccountAdapter();
+        recyclerView.setAdapter(adapter);
 
         viewModel = new ViewModelProvider(this).get(AccountViewModel.class);
         viewModel.getAllAccounts().observe(this, new Observer<List<Account>>() {
             @Override
             public void onChanged(List<Account> accounts) {
                 //updating he recycler view
-                Toast.makeText(MainActivity.this, "updating recycler view", Toast.LENGTH_SHORT).show();
+                adapter.setAccounts(accounts);
             }
         });
     }
