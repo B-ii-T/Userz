b257542ba575d71e79e22cea3c9ce0ee31f6d955
Fatah <bnb.fatah@gmail.com>
2022-08-22 22:56:30 +0100

    close settings on click

--- app/src/main/java/com/bit/userz/SettingsActivity.java
+++ app/src/main/java/com/bit/userz/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.bit.userz;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import maes.tech.intentanim.CustomIntent;
 
 public class SettingsActivity extends AppCompatActivity {
+    private ImageView closeOptionsBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        closeOptionsBtn = findViewById(R.id.close_options_btn);
+        closeOptionsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     @Override
